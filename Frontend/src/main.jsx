3c09a32c73d5293664f2d5f13856e60dccb5c848
riyaz ahmad <riyaz.ahmad@indusnet.co.in>
2023-09-14 22:44:21 +0530

    redux, toolkit installation and setup

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import authReducer from "./Redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+import { curryGetDefaultMiddleware, getDefaultMiddleware } from "@reduxjs/toolkit/dist/getDefaultMiddleware.js";
+// import persistReducer from "redux-persist/es/persistReducer";
+
+const persistConfig= {key:"root", storage, version:1};
+const persistedReducer= persistReducer(persistConfig,authReducer)
+
+const store= configureStore({
+  reducer:persistedReducer,
+  middleware:(curryGetDefaultMiddleware)=>
+  getDefaultMiddleware({serializableCheck:{ignoreActions:
+  [FLUSH,REHYDRATE,PURGE,PAUSE,PERSIST,REGISTER]}})
+})
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading= {null} persistor={persistStore(store)}>
+
+      
+      <App />
+      </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
