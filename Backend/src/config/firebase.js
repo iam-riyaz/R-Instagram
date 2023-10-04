b47662acf116cb19f498901772d8b6641d1593a7
iam-riyaz <97458162+iam-riyaz@users.noreply.github.com>
2023-10-04 22:07:43 +0530

    firebase file upload method added for testing

--- Backend/src/config/firebase.js
+++ Backend/src/config/firebase.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: process.env.APIKEY,
   authDomain: process.env.AUTHDOMAIN,
   projectId: process.env.PROJECTID,
@@ -21,7 +21,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 
 
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,82 +1,96 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
 
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import dotenv from "dotenv"
-import multer from 'multer';
-import helmet from "helmet"
-import morgan from "morgan"
-import path from 'path';
-
-
-
-import { fileURLToPath } from 'url';
-import { connectDB } from './config/db.js';
-import {register} from "./controller/user.controller.js"
-import {userRouter} from "./routes/user.route.js"
-import { verifyToken } from './middleware/authorization.js';
-import { postsRouter } from './routes/posts.route.js';
-import { User } from './models/User.js';
-import { posts, users } from './data/index.js';
-import { Post } from './models/Post.js';
-import { createPost } from './controller/posts.controller.js';
-
-
+import { fileURLToPath } from "url";
+import { connectDB } from "./config/db.js";
+import { register } from "./controller/user.controller.js";
+import { userRouter } from "./routes/user.route.js";
+import { verifyToken } from "./middleware/authorization.js";
+import { postsRouter } from "./routes/posts.route.js";
+import { User } from "./models/User.js";
+import { posts, users } from "./data/index.js";
+import { Post } from "./models/Post.js";
+import { createPost } from "./controller/posts.controller.js";
+import { firebaseStorage } from "./config/firebase.js";
+import { ref, uploadBytes } from "firebase/storage";
 
 // configuration------
-const __filename=fileURLToPath(import.meta.url);
-const __dirname=path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
-const app= express();
-app.use(express.json({limit:"30mb", extended:true}))
-app.use(express.urlencoded({limit:"30mb",extended:true}))
-app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}))
-app.use(morgan("common"))
-app.use(cors())
-app.use("/assets", express.static(path.join(__dirname,"public/assets")));
-app.use(express.static('public'));
+const app = express();
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+app.use(express.static("public"));
 
 // FILE STORAGE --------------------------------
-const storage= multer.diskStorage({
-    destination:function(req,file, cd){
-        cd(null, "./public/assets");
-    },
-    filename:function(req, file,cd){
-        cd(null,file.originalname);
-    }
+const storage = multer.diskStorage({
+  destination: function (req, file, cd) {
+    cd(null, "./public/assets");
+  },
+  filename: function (req, file, cd) {
+    cd(null, file.originalname);
+  },
 });
 
- export const upload=multer({storage})
-// const upload=multer({storage:multer.memoryStorage()})
+//  export const upload=multer({storage})
+const upload = multer({ storage: multer.memoryStorage() });
 
-app.get("/", async(req,res)=>{
-    console.log("Uploading")
-    res.status(200).send("route is working")
-})
+app.get("/", async (req, res) => {
+  console.log("Uploading");
+  res.status(200).send("route is working");
+});
 
-// ROUTES WITH FILE------------   
-app.post("/register", upload.single("picture"), register)
+// ROUTES WITH FILE------------
+app.post("/register", upload.single("picture"), register);
 
-app.post("/posts/create" , verifyToken, upload.single("picture"),createPost)
+app.post("/posts/create", verifyToken, upload.single("picture"), createPost);
 
 // ROUTES for USER
-app.use("/users", userRouter)
+app.use("/users", userRouter);
 
 // ROUTE of POST
-app.use("/posts", postsRouter )
+app.use("/posts", postsRouter);
 
+// TESTING for Firebase
+app.post("/test", upload.single("img"), async (req, res) => {
+  try {
+    const storageRef = ref(firebaseStorage, `/test/${req.file.originalname}`);
 
+    uploadBytes(storageRef, req.file.buffer)
+      .then((e) => {
+        let fullpath = e.metadata.fullPath;
+        console.log({ fullpath });
 
-const PORT = process.env.PORT || 2001
+        res.send({ "data":e });
+      })
+      .catch((err) => {
+        res.send({ err });
+      });
+  } catch (err) {
+    res.send({ err });
+  }
+});
 
-connectDB().then(()=>{
+const PORT = process.env.PORT || 2001;
 
-    app.listen( PORT, ()=>{
-        console.log(`server is listening on port ${PORT}`)
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}`);
 
     //    ONE TIME DATA INSERT
-        // User.insertMany(users)
-        // Post.insertMany(posts)
-    })
-})
+    // User.insertMany(users)
+    // Post.insertMany(posts)
+  });
+});
