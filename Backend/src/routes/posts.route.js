d1fc22aacc5878f2813e6e08cebb3981b1984818
iam-riyaz <97458162+iam-riyaz@users.noreply.github.com>
2023-10-07 17:46:42 +0530

    user and post route updated

--- Backend/src/routes/posts.route.js
+++ Backend/src/routes/posts.route.js
@@ -1,15 +1,12 @@
-import express from "express"
-import { verifyToken } from '../middleware/authorization.js';
-import * as postsController from "../controller/posts.controller.js"
+import express from "express";
+import { verifyToken } from "../middleware/authorization.js";
+import * as postsController from "../controller/posts.controller.js";
 
-export const postsRouter= express.Router();
-
-
+export const postsRouter = express.Router();
 
 // READ
-postsRouter.get("/",verifyToken, postsController.getFeedPosts)
-postsRouter.get("/:userId/", verifyToken, postsController.getUserPosts)
-
+postsRouter.get("/", verifyToken, postsController.getFeedPosts);
+postsRouter.get("/:userId/", verifyToken, postsController.getUserPosts);
 
 // UPDATE
-postsRouter.patch("/:id/like",verifyToken, postsController.likePost)
+postsRouter.patch("/:id/like", verifyToken, postsController.likePost);
--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,19 +1,19 @@
-
-import express from "express"
-import * as userController from "../controller/user.controller.js"
+import express from "express";
+import * as userController from "../controller/user.controller.js";
 import { verifyToken } from "../middleware/authorization.js";
 
-
-export const userRouter= express.Router();
+export const userRouter = express.Router();
 
 // user LOGIN
-userRouter.post("/login", userController.login)
+userRouter.post("/login", userController.login);
 
 // READ
-userRouter.get("/:id", verifyToken, userController.getUser)
-userRouter.get("/:id/friends", verifyToken, userController.getUserFriends)
+userRouter.get("/:id", verifyToken, userController.getUser);
+userRouter.get("/:id/friends", verifyToken, userController.getUserFriends);
 
 // UPDATE
-userRouter.patch("/:id/:friendId", verifyToken, userController.addRemoveFriends);
-
-
+userRouter.patch(
+  "/:id/:friendId",
+  verifyToken,
+  userController.addRemoveFriends
+);
